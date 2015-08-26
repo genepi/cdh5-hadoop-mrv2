743bf32229aeb5eea13b6f188596aadc6fb07c9d
Sebastian Schoenherr <c703456@uibk.ac.at>
2015-08-26 22:42:11 +0200

    corrected path to jar

--- conf/execute-wordcount.sh
+++ conf/execute-wordcount.sh
@@ -1,3 +1,3 @@
 sudo -u cloudgene hadoop fs -mkdir input
 sudo -u cloudgene hadoop fs -put /etc/hadoop/conf/*.xml input
-sudo -u cloudgene /usr/bin/hadoop jar /usr/lib/hadoop-0.20-mapreduce/hadoop-examples.jar wordcount input output
+sudo -u cloudgene /usr/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar wordcount input output
