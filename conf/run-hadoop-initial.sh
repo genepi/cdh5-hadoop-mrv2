ae9d1968802ed0b11d9f77d6ad4781c14541e98d
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2015-12-07 15:38:48 +0100

    updating config

--- conf/run-hadoop-initial.sh
+++ conf/run-hadoop-initial.sh
@@ -5,11 +5,9 @@
 for x in `cd /etc/init.d ; ls hadoop-hdfs-*` ; do sudo service $x start ; done
 sudo /usr/bin/init-hdfs.sh
 
-# adapt MapReduce configuration. this needs some thinking. old parameters are not there anymore
 # http://blog.cloudera.com/blog/2013/11/migrating-to-mapreduce-2-on-yarn-for-operators/
 #sudo /usr/bin/adapt-mapred-config.sh
 
-sudo -u hdfs hadoop fs -ls -R /
 sudo service hadoop-yarn-resourcemanager start
 sudo service hadoop-yarn-nodemanager start
 sudo service hadoop-mapreduce-historyserver start
